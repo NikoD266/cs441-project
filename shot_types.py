901ddcc79ad8d6d4cc214933eaf4c9cf255653cb
EJPotter8 <112223564+EJPotter8@users.noreply.github.com>
2025-10-08 10:38:28 -0400

    Add files via upload

---
+++ shot_types.py
@@ -0,0 +1,103 @@
+import pandas as pd
+
+data = pd.read_csv("C:\\Users\\ejpot\\Downloads\\charting-m-stats-ShotTypes.csv")
+
+pd.set_option("display.max_columns", None)
+pd.set_option("display.max_rows", None)
+pd.set_option("display.max_colwidth", None)
+pd.set_option("display.width", None)
+
+data = data[data["match_id"].astype(str).str.startswith("2")]
+
+shots_pivot = data.pivot_table(
+    index="player",
+    columns="row",
+    values="shots",
+    aggfunc="sum",
+    fill_value=0
+).reset_index()
+
+pivot_extended = data.pivot_table(
+    index="player",
+    columns="row",
+    values=["shots", "winners", "induced_forced", "unforced"],
+    aggfunc="sum",
+    fill_value=0
+)
+
+pivot_extended.columns = [f"{shot}_{val}" for val, shot in pivot_extended.columns]
+pivot_extended = pivot_extended.reset_index()
+
+cols = pivot_extended.columns.tolist()
+cols_sorted = ['player'] + sorted([col for col in cols if col != 'player'])
+pivot_extended = pivot_extended[cols_sorted]
+
+shot_types = set(col.split('_')[1] for col in pivot_extended.columns if col.startswith("shots_"))
+
+for shot in shot_types:
+    shots_col = f"shots_{shot}"
+    winners_col = f"winners_{shot}"
+    induced_col = f"induced_forced_{shot}"
+    unforced_col = f"unforced_{shot}"
+    other_col = f"{shot}_other"
+
+    # Ensure all required columns exist before computing
+    if all(col in pivot_extended.columns for col in [shots_col, winners_col, induced_col, unforced_col]):
+        pivot_extended[other_col] = (
+            pivot_extended[shots_col]
+            - pivot_extended[winners_col]
+            - pivot_extended[induced_col]
+            - pivot_extended[unforced_col]
+        )
+
+
+print(pivot_extended[0:10])
+
+
+shot_cols = [col for col in shots_pivot.columns if col not in ["player"]]
+
+for x in shot_cols:
+    if_col = x + "_induced_forced"
+    shots_col = x + "_shots"
+    unf_col = x + "_unforced"
+    win_col = x + "_winners"
+
+
+outcome_cols = ['winners', 'induced_forced', 'unforced']
+
+shots_percent = shots_pivot.copy()
+shots_percent[shot_cols] = shots_percent[shot_cols].div(shots_percent["Total"], axis=0) * 100
+
+# print(shots_percent)
+
+sum_cols = ["shots", "pt_ending", "winners", "induced_forced", "unforced"]
+
+summed_df = data.groupby(["player", "row"], as_index=False)[sum_cols].sum()
+
+print(summed_df[0:50])
+
+summed_df['winner_pct'] = (summed_df['winners'] / summed_df['shots']) * 100
+summed_df['induced_forced_pct'] = (summed_df['induced_forced'] / summed_df['shots']) * 100
+summed_df['unforced_pct'] = (summed_df['unforced'] / summed_df['shots']) * 100
+
+summed_df['non_pt_ending'] = summed_df['shots'] - summed_df['pt_ending']
+summed_df['non_pt_ending_pct'] = (summed_df['non_pt_ending'] / summed_df['shots']) * 100
+
+summed_df[['winner_pct', 'induced_forced_pct', 'unforced_pct', 'non_pt_ending_pct']] = \
+    summed_df[['winner_pct', 'induced_forced_pct', 'unforced_pct', 'non_pt_ending_pct']].fillna(0)
+
+total_shots_df = summed_df[summed_df["row"] == "Total"][["player", "shots"]].rename(columns={"shots": "total_player_shots"})
+
+summed_df = pd.merge(summed_df, total_shots_df, on="player", how="left")
+
+summed_df["shot_usage_pct"] = (summed_df["shots"] / summed_df["total_player_shots"]) * 100
+
+long_df = summed_df[[
+    "player", "row", "shot_usage_pct", "winner_pct", "induced_forced_pct", "unforced_pct", "non_pt_ending_pct"
+]].rename(columns={"row": "shot_type"})
+
+print(long_df.head(50))
+
+print(long_df.shape)
+
+csv_data = long_df.to_csv('player_shots_data.csv')
